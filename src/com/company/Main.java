a177f6992b019e196628ba9abf2f219a30a8ea93
coldblade2000 <diegogranada7@gmail.com>
2018-04-24 10:06:50 -0500

    h6

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,26 @@
 package com.company;
 
-public class Main {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+import java.util.Scanner;
+
+public class Main extends Application{
  // https://imgur.com/a/QEhEX6b 4th grade
+    // https://github.com/javaterminal/TerminalFX
     public static void main(String[] args) {
+        boolean continueGame = true;
+        System.out.println("Welcome to the Anglo Tech Day!");
+        Scanner scan = new Scanner(System.in);
+        while(continueGame){
+            System.out.println("Please enter the characters in order without any spaces or quotes. (e.g.: 87g4fw");
+            String input = scan.nextLine();
+
+        }
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
 
     }
 }
